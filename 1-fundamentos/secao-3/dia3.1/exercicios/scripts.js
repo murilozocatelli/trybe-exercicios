dc27ba3ca716bb1356f8deeda7b18682d00d85e5
Murilo Zocatelli <murilo.zocatelli@gmail.com>
2023-04-04 12:26:38 -0300

    testando primeiras variaveis

---
+++ 1-fundamentos/secao-3/dia3.1/exercicios/scripts.js
@@ -0,0 +1,13 @@
+let movie = "Avengers"; // string literal
+let score = 10.89; // number literal 
+let isValid = true; // boolean
+let name; // undefined
+let color = null; //redefinição
+
+
+// +, -, *, /, **, 
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(movie)
