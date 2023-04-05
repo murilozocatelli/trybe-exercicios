ac680ef7fe95d0a530a644b67ad145319d1eebf7
Murilo <murilo.zocatelli@gmail.com>
2023-04-05 19:16:08 -0300

    fazendo alguns exercicios

---
+++ 1-fundamentos/secao-3/dia3.1/exercicios/scripts.js
@@ -0,0 +1,54 @@
+let myName = "Murilo";
+console.log(myName);
+const age = 30;
+console.log(age);
+let birthCity = "Campo Mourão"
+console.log(birthCity);
+let birthYear = "1995";
+console.log(birthYear);
+
+// Dois tipos: referência e valores
+//  tipagem dinâmica
+
+let movie = "Avenger" // string literal
+let score = 8.92 // number literal
+let isValid = true // boolean
+let name; // undefined
+let color = null; // redefinition
+
+/* operações aritméticas
++ , - , * , / , ** , ++ , -- */
+
+let a = 0;
+console.log(a++); // 0, pois o console.log ocorre antes do incremento.
+// menos nítido
+
+let a = 0;
+console.log(a += 1); // 1, pois a atribuição ocorre antes
+
+let a = 0;
+a += 1;
+console.log(a); // leitura mais nítida */
+
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientInfo); //typeof: object
+console.log(typeof agez); // typeof: undefined
+
+//
+
+let base = 5
+let height = 8
+let area = (base * height)
+console.log(area);
+let perimetro = (2*base + 2*height);
+console.log(perimetro);
+
+//
+
