50f42bf169573672ce499214dfab4ed09d4800b6
Murilo <murilo.zocatelli@gmail.com>
2023-04-09 11:17:57 -0300

    iniciando array

---
+++ 1-fundamentos/secao-3/dia3.2/scripts.js
@@ -0,0 +1,18 @@
+/* let pizzas = ['4 queijos', 'Frango com catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+
+pizzas.push('Peito de peru'); 
+pizzas.push('Gratinada');
+pizzas.unshift('Peperoni')
+pizzas.pop();
+pizzas.shift();
+
+for (let index = 0; index < pizzas.length; index +=1){ 
+    console.log(pizzas[index])
+}
+
+let indexofMarguerita = pizzas.indexOf('Marguerita');
+console.log(indexofMarguerita); */
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+console.log(menu.sort());
