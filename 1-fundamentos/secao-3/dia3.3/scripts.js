ff5c2acd697137bf5fa84fd602fb6137f3eadfd4
Murilo <murilo.zocatelli@gmail.com>
2023-04-09 20:52:37 -0300

    começo do dia 3

--- 1-fundamentos/secao-3/dia3.3/scripts.js
+++ 1-fundamentos/secao-3/dia3.3/scripts.js
@@ -1,33 +1,49 @@
-function cumprimentaCliente(cliente) {
-    return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
-  }
+// function cumprimentaCliente(cliente) {
+//     return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
+//   }
   
-  console.log(cumprimentaCliente('Isaac')) 
+//   console.log(cumprimentaCliente('Isaac')) 
 
-function somaValor(saldo) {
-    return saldo + 100
-}
-console.log(somaValor(200))
+// function somaValor(saldo) {
+//     return saldo + 100
+// }
+// console.log(somaValor(200))
 
-function subtraiValor(saldo) {
-    return saldo - 100
-}
-console.log(subtraiValor(400))
+// function subtraiValor(saldo) {
+//     return saldo - 100
+// }
+// console.log(subtraiValor(400))
 
-function multiplicaValor(saldo) {
-    return saldo * 3
-}
-console.log(multiplicaValor(300))
+// function multiplicaValor(saldo) {
+//     return saldo * 3
+// }
+// console.log(multiplicaValor(300))
 
-function divideValor(saldo) {
-    return saldo / 10
-}
-console.log(divideValor(1000))
+// function divideValor(saldo) {
+//     return saldo / 10
+// }
+// console.log(divideValor(1000))
 
-let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+// let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
-function cumprimentaCliente(cliente) {
-  return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
-}
+// function cumprimentaCliente(cliente) {
+//   return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
+// }
 
-console.log(cumprimentaCliente(clientesTrybeBank));
+// console.log(cumprimentaCliente(clientesTrybeBank));
+
+//
+
+// let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+// function novoCliente (nome) {
+//     if (typeof nome === 'string') {
+//     clientesTrybeBank.push(nome);
+//     return nome
+//     }   else {return 'erro'
+// }
+// } 
+// console.log(novoCliente('Márcio'))
+// console.log(novoCliente(5))
+// console.log(novoCliente('5'))
+// console.log(clientesTrybeBank)
