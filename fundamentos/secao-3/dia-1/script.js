27f2866ee999a95852b326f0cea82f45e9bcd6d8
Murilo Zocatelli <murilo.zocatelli@gmail.com>
2023-08-17 15:54:30 -0300

    feito penando o exercicio 7

--- fundamentos/secao-3/dia-1/script.js
+++ fundamentos/secao-3/dia-1/script.js
@@ -66,3 +66,121 @@
 // } else {
 //     console.log(false)
 // }
+
+// let salarioBruto = 5000
+// let aliquotaINSS; // Fazer If pra descobrir
+// let salarioBase;
+// let deducao; // Fazer if a partir do salarioBase para descobrir
+// let aliquotaImpostoDeRenda; // Fazer if a partir do salarioBase para descobrir
+// let impostoDeRenda ;// Fazer if a partir do salarioBase pra descobrir
+// let salarioLiquido;
+
+// if (typeof salarioBruto !== 'number' || salarioBruto < 0) {
+//     console.log ('A váriavel salarioBruto deve ser um número positivo')
+// } else if (salarioBruto <= 1556.94) {
+//     aliquotaINSS = (8 / 100) * salarioBruto
+// } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+//     aliquotaINSS = 9 / 100 * salarioBruto
+// } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+//     aliquotaINSS = 11 / 100 * salarioBruto
+// } else if (salarioBruto > 5189.82) {
+//     aliquotaINSS = 11 / 100 * salarioBruto 
+//     if (aliquotaINSS > 570.88) {
+//         aliquotaINSS = 570.88
+//     }
+
+//     salarioBase = salarioBruto - aliquotaINSS;
+// } 
+// // ... (seu código anterior)
+
+// if (salarioBase <= 1903.98) {
+//     impostoDeRenda = 0;
+// } else if (salarioBase > 1903.99 && salarioBase <= 2826.65) {
+//     aliquotaImpostoDeRenda = 7.5 / 100;
+//     deducao = 142.80;
+//     impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+// } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+//     aliquotaImpostoDeRenda = 15 / 100;
+//     deducao = 354.80;
+//     impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+// } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+//     aliquotaImpostoDeRenda = 22.5 / 100;
+//     deducao = 636.13;
+//     impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+// } else if (salarioBase > 4664.68) {
+//     aliquotaImpostoDeRenda = 27.5 / 100;
+//     deducao = 869.36;
+//     impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+// }
+
+// // ... (seu código posterior)
+
+
+
+// salarioLiquido = salarioBase - impostoDeRenda;
+
+//  console.log('Salário bruto: R$' + salarioBruto)
+//  console.log('Alíquota INSS: R$' + aliquotaINSS)
+//  console.log('Salário base: R$' + salarioBase)
+//  console.log('Dedução do Imposto de Renda : R$' + deducao)
+//  console.log('Alíquota Imposto de Renta: R$' + aliquotaImpostoDeRenda)
+//  console.log('Imposto de Renda: R$' + impostoDeRenda)
+//  console.log('Salário líquido: R$' + salarioLiquido)
+
+let salarioBruto = 5000;
+let aliquotaINSS; // Fazer If pra descobrir
+let salarioBase;
+let deducao; // Fazer if a partir do salarioBase para descobrir
+let aliquotaImpostoDeRenda; // Fazer if a partir do salarioBase para descobrir
+let impostoDeRenda; // Fazer if a partir do salarioBase pra descobrir
+let salarioLiquido;
+
+if (typeof salarioBruto !== 'number' || salarioBruto < 0) {
+    console.log('A váriavel salarioBruto deve ser um número positivo');
+} else if (salarioBruto <= 1556.94) {
+    aliquotaINSS = (8 / 100) * salarioBruto;
+    salarioBase = salarioBruto - aliquotaINSS;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    aliquotaINSS = 9 / 100 * salarioBruto;
+    salarioBase = salarioBruto - aliquotaINSS;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    aliquotaINSS = 11 / 100 * salarioBruto;
+    salarioBase = salarioBruto - aliquotaINSS;
+} else if (salarioBruto > 5189.82) {
+    aliquotaINSS = 11 / 100 * salarioBruto;
+    if (aliquotaINSS > 570.88) {
+        aliquotaINSS = 570.88;
+    }
+    salarioBase = salarioBruto - aliquotaINSS;
+}
+
+if (salarioBase <= 1903.98) {
+    impostoDeRenda = 0;
+} else if (salarioBase > 1903.99 && salarioBase <= 2826.65) {
+    aliquotaImpostoDeRenda = 7.5 / 100;
+    deducao = 142.80;
+    impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    aliquotaImpostoDeRenda = 15 / 100;
+    deducao = 354.80;
+    impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    aliquotaImpostoDeRenda = 22.5 / 100;
+    deducao = 636.13;
+    impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+} else if (salarioBase > 4664.68) {
+    aliquotaImpostoDeRenda = 27.5 / 100;
+    deducao = 869.36;
+    impostoDeRenda = aliquotaImpostoDeRenda * salarioBase - deducao;
+}
+
+salarioLiquido = salarioBase - impostoDeRenda;
+
+console.log('Salário bruto: R$' + salarioBruto);
+console.log('Alíquota INSS: R$' + aliquotaINSS);
+console.log('Salário base: R$' + salarioBase);
+console.log('Dedução do Imposto de Renda: R$' + deducao);
+console.log('Alíquota Imposto de Renda: ' + (aliquotaImpostoDeRenda * 100) + '%');
+console.log('Imposto de Renda: R$' + impostoDeRenda);
+console.log('Salário líquido: R$' + salarioLiquido);
+
