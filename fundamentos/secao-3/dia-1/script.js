cbaadb4d538e1ebb1d286680c869984ea7820300
Murilo Zocatelli <murilo.zocatelli@gmail.com>
2023-08-17 14:43:38 -0300

    feito exercicio 6

--- fundamentos/secao-3/dia-1/script.js
+++ fundamentos/secao-3/dia-1/script.js
@@ -56,3 +56,13 @@
 //         console.log('defina a peça')
 //         break;
 // }
+
+// let num1 = 1;
+// let num2 = 5;
+// let num3 = 3;
+
+// if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
