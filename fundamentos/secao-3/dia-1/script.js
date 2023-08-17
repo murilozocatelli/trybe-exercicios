bf7ef45dc9f7adc1e6703a16eadec20fc4463f7c
Murilo Zocatelli <murilo.zocatelli@gmail.com>
2023-08-17 14:27:17 -0300

    feito exercicios de 1 a 5

---
+++ fundamentos/secao-3/dia-1/script.js
@@ -0,0 +1,37 @@
+// let num1 = 14;
+// let num2 = 9;
+// let maiorNumero;
+
+// if (num1 > num2) {
+//     maiorNumero = num1
+// } else if (num2 > num1) {
+//     maiorNumero = num2
+// }
+// console.log(maiorNumero);
+
+// let num1 = 6;
+// let num2 = 4;
+// let num3 = 2;
+// let maiorNumero;
+
+// if (num1 > num2 && num1 > num3) {
+//     maiorNumero = num1
+// }   else if (num2 > num1 && num2 > num3) {
+//     maiorNumero = num2
+// } else if (num3 > num1 && num3 > num2) {
+//     maiorNumero = num3
+// }
+// console.log(maiorNumero);
+
+// let ang1 = '30';
+// let ang2 = 100;
+// let ang3 = 40;
+
+
+// if (typeof ang1 !== "number" || typeof ang2 !== 'number' || typeof ang3 !== 'number') {
+//     console.log("Erro! as variaveis angs devem ser números!")
+// } else if (ang1 + ang2 + ang3 == 180) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
