63c9fb9b8f73ef316c7215f24315f9c5de057273
Murilo Zocatelli <murilo.zocatelli@gmail.com>
2023-10-29 23:49:12 -0300

    feito o estudo teórico, próximo passo é fazer exercício

--- fundamentos/secao-4/dia-2/script.js
+++ fundamentos/secao-4/dia-2/script.js
@@ -1,31 +1,3 @@
-// // Arquivo script.js
-
-// // Cria um novo elemento <li> e o armazena na variável newElement
-// let newElement = document.createElement('li');
-
-// // Acessa o novo elemento armazenado em newElement e altera seu conteúdo
-// newElement.innerHTML = 'Um texto para o novo elemento';
-
-// // Recupera o elemento que será o pai de newElement e o armazena na variável parent
-// let parent = document.getElementById('parent');
-
-// // Insere o novo elemento como filho de parent
-// parent.appendChild(newElement);
-
-let irmaoDeOnde = document.createElement('p');
-irmaoDeOnde.innerHTML = 'testando <b> strong </b>';
-let parent = document.getElementById('elementoOndeVoceEsta').parentElement;
-parent.appendChild(irmaoDeOnde);
-
-let filhoDeOnde = document.createElement('section');
-filhoDeOnde.innerText = 'filho do Donde'
-let paiDeOnde = document.getElementById('elementoOndeVoceEsta');
-paiDeOnde.appendChild(filhoDeOnde);
-
-let bisneto = document.createElement('p');
-bisneto.innerHTML = '<i> itálico ?</i>';
-let paiDoBisneto = document.getElementById('primeiroFilhoDoFilho');
-paiDoBisneto.appendChild(bisneto);
-
-let tioAvoDoBisneto = bisneto.parentElement.parentElement.nextElementSibling;
-
+const whereAreYou = document.getElementById('where-are-you');
+const h2 = whereAreYou.firstElementChild;
+whereAreYou.removeChild(h2); 
